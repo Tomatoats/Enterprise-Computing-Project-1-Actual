5b0c26af5d80376ba3503b02693283b02b21091d
Alexys Veloz <alexys.veloz.fl@gmail.com>
2023-09-12 18:21:28 -0400

    Finally got java FX to work with IntelliJ 2023

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module actual.server_store {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens actual.server_store to javafx.fxml;
+    exports actual.server_store;
+}
